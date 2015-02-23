1f594451829e648b681cc5fe3e08ec6ec3b60425
Andreas Rubik <rubikandreas@gmail.com>
2015-02-23 20:08:11 +0100

    added a raw structure for the program

--- myfind/src/main.c
+++ myfind/src/main.c
@@ -1,19 +1,53 @@
+#include <sys/stat.h>
+#include <dirent.h>
+#include <string.h>
+#include <errno.h>
 #include "parseopt.h"
 
 int main(int argc, char* argv[]) {
 	Option first;
+
 	char* startdir = (strncmp(".", argv[1], 1) == 0 || strncmp("/", argv[1], 1) == 0) ? argv[1] : ".";
 	parseopts(argc, argv, &first);
 
-	printf("Startdir: %s\n", startdir);
-	while (first.next != NULL) {
-		printf("%s ", first.next->name);
-		if (first.next->argument != NULL)
-			printf("%s\n", first.next->argument);
-		else
-			printf("\n");
-		first = *first.next;		
+	pDir = opendir(startdir);
+	if (pDir == NULL) {
+		printf("Cannot open directory %s!\n", startdir);
+		return EXIT_FAILURE;
 	}
 
-	return 0;
+	return EXIT_SUCCESS;
+}
+
+void do_dir(const char* dir_name, const char* const* parms) {
+	DIR *pDir;
+	pDir = opendir(dir_name);
+	struct dirent *pDirentry;
+	struct stat pStat;
+	char file[512];
+	int status;
+
+	while ((pDirentry = readdir(pDir)) != NULL) {
+		strcpy(file, dirname);
+		strcat(file, "/");
+		strcat(file, pDirentry->d_name);
+		status = stat(file, &pStat);
+
+		if(status == -1)
+			printf("ERROR: %s\n", strerror(errno));
+
+		if S_ISREG(pStat.st_mode) {
+				//TODO: Wenn es ein regular File ist.
+		}
+		
+		if S_ISDIR(pStat.st_mode) {
+				//TODO: Wenn es ein Directory ist.
+		}
+	}
+
+	closedir(pDir);
+}
+
+void do_file(const char* file_name, const char* const* parms) {
+
 }
