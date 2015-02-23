273a1d80dd205cca3de7e64e9b17b87b15a0c36c
Andreas Rubik <rubikandreas@gmail.com>
2015-02-23 20:16:16 +0100

    modified function headers

--- myfind/src/main.c
+++ myfind/src/main.c
@@ -4,28 +4,32 @@
 #include <errno.h>
 #include "parseopt.h"
 
+void do_dir(const char* dir_name, Option* first);
+void do_file(const char* file_name, Option* first);
+
 int main(int argc, char* argv[]) {
 	Option first;
 
 	char* startdir = (strncmp(".", argv[1], 1) == 0 || strncmp("/", argv[1], 1) == 0) ? argv[1] : ".";
 	parseopts(argc, argv, &first);
 
-	pDir = opendir(startdir);
-	if (pDir == NULL) {
-		printf("Cannot open directory %s!\n", startdir);
-		return EXIT_FAILURE;
-	}
+	do_dir(startdir, &first);
 
 	return EXIT_SUCCESS;
 }
 
-void do_dir(const char* dir_name, const char* const* parms) {
+void do_dir(const char* dir_name, Option* first) {
 	DIR *pDir;
 	pDir = opendir(dir_name);
 	struct dirent *pDirentry;
 	struct stat pStat;
 	char file[512];
 	int status;
+
+	if (pDir == NULL) {
+		printf("Cannot open directory %s!\n", startdir);
+		return EXIT_FAILURE;
+	}
 
 	while ((pDirentry = readdir(pDir)) != NULL) {
 		strcpy(file, dirname);
@@ -39,7 +43,7 @@ void do_dir(const char* dir_name, const char* const* parms) {
 		if S_ISREG(pStat.st_mode) {
 				//TODO: Wenn es ein regular File ist.
 		}
-		
+
 		if S_ISDIR(pStat.st_mode) {
 				//TODO: Wenn es ein Directory ist.
 		}
@@ -48,6 +52,6 @@ void do_dir(const char* dir_name, const char* const* parms) {
 	closedir(pDir);
 }
 
-void do_file(const char* file_name, const char* const* parms) {
+void do_file(const char* file_name, Option* first) {
 
 }
