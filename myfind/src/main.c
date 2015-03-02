3ac62b3c93da060cc12af8f47d66dfa55b933fa6
Andreas Rubik <rubikandreas@gmail.com>
2015-03-02 20:44:06 +0100

    -name is now working

--- myfind/src/main.c
+++ myfind/src/main.c
@@ -2,10 +2,12 @@
 #include <dirent.h>
 #include <string.h>
 #include <errno.h>
+#include <libgen.h>
+#include <fnmatch.h>
 #include "parseopt.h"
 
-void do_dir(const char* dir_name, Option* first);
-void do_file(const char* file_name, Option* first);
+void do_dir(const char* dir_path, Option* first);
+void do_file(const char* file_path, Option* first);
 
 int main(int argc, char* argv[]) {
 	Option* first = (Option *) malloc(sizeof(Option));
@@ -22,21 +24,21 @@ int main(int argc, char* argv[]) {
 	return EXIT_SUCCESS;
 }
 
-void do_dir(const char* dir_name, Option* first) {
+void do_dir(const char* dir_path, Option* first) {
 	DIR *pDir;
 	DIR *pSubDir;
 	struct dirent *pDirentry;
 	struct stat pStat;
 	char file[512];
 	int status;
-	pDir = opendir(dir_name);
+	pDir = opendir(dir_path);
 
 	if (pDir == NULL) {
-		printf("Cannot open directory %s!\n", dir_name);
+		printf("Cannot open directory %s!\n", dir_path);
 	}
 
 	while ((pDirentry = readdir(pDir)) != NULL) {
-		strcpy(file, dir_name);
+		strcpy(file, dir_path);
 		strcat(file, "/");
 		strcat(file, pDirentry->d_name);
 		status = stat(file, &pStat);
@@ -49,29 +51,39 @@ void do_dir(const char* dir_name, Option* first) {
 		}
 
 		if S_ISDIR(pStat.st_mode) {
-				printf("%s\n", dir_name);
-				/*Überspringt das rekursive durchlaufen der . und .. Ordner*/
-				if (!(strncmp(pDirentry->d_name, ".", 1) == 0 || strncmp(pDirentry->d_name, "..", 2) == 0)) {
-					pSubDir = opendir(file);
-					do_dir(file, first);
-					closedir(pSubDir);
-				}
+			/*printf("%s\n", dir_path);*/
+			/*Überspringt das rekursive durchlaufen der . und .. Ordner*/
+			if (!(strncmp(pDirentry->d_name, ".", 1) == 0 || strncmp(pDirentry->d_name, "..", 2) == 0)) {
+				pSubDir = opendir(file);
+				do_dir(file, first);
+				closedir(pSubDir);
 			}
 		}
-
+	}
 	closedir(pDir);
 }
 
-void do_file(const char* file_name, Option* first) {
+void do_file(const char* file_path, Option* first) {
 	Option* current = first;
-	struct stat pStat;
+	/*Den Filename für die Namensüberprüfung extrahieren mit basename()*/
+	char* temp = malloc(strlen(file_path)+1);
+	char* file_name = NULL;
+	strcpy(temp, file_path);
+	file_name = basename(temp);
 	/*Beinhaltet alle Informationen zu dem File*/
 	/*Folien "Linux Filesystem" ab Seite 16.*/
-	stat(file_name, &pStat);
+	struct stat pStat;
+	stat(file_path, &pStat);
 
 	while(current->next != NULL) {
-		if (strncmp(current->name, "-print", 6) == 0)
-			printf("%s\n", file_name);
+		if (strncmp(current->name, "-name", 5) == 0) {
+			if (fnmatch(current->argument, file_name, 0) == FNM_NOMATCH) {
+				break;
+			}
+		}
+		else if (strncmp(current->name, "-print", 6) == 0) {
+			printf("%s\n", file_path);
+		}
 		current = current->next;
 	}
 }
