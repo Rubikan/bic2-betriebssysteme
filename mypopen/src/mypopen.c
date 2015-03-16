6a80f06ba8d66fa8330a4eedbca947a607ad999f
Rubikan <rubikandreas@gmail.com>
2015-03-16 20:04:44 +0100

    added files for the exercise

---
+++ mypopen/src/mypopen.c
@@ -0,0 +1,24 @@
+/**
+ *
+ * @file mypopen.c
+ * Betriebssysteme Betriebssysteme myfind File.
+ * Beispiel 2
+ *
+ * @author Andreas Rubik <andreas.rubik@technikum-wien.at>
+ * @author Anna Glock <ic14b070@technikum-wien.at>
+ * @author Anna Huka <ic14b043@technikum-wien.at>
+ *
+ * @version 1.0
+ * @date 2015/03/13
+ *
+ */
+
+#include "mypopen.h"
+
+FILE *popen(const char *command, const char *type) {
+
+}
+
+int pclose(FILE *stream) {
+  
+}
---
+++ mypopen/src/mypopen.h
@@ -0,0 +1,24 @@
+/**
+ *
+ * @file mypopen.h
+ * Betriebssysteme Betriebssysteme myfind File.
+ * Beispiel 2
+ *
+ * @author Andreas Rubik <andreas.rubik@technikum-wien.at>
+ * @author Anna Glock <ic14b070@technikum-wien.at>
+ * @author Anna Huka <ic14b043@technikum-wien.at>
+ *
+ * @version 1.0
+ * @date 2015/03/16
+ *
+ */
+
+#ifndef MYPOPEN_H
+#define MYPOPEN_H
+
+#include <unistd.h>
+
+FILE *popen(const char *command, const char *type);
+int pclose(FILE *stream);
+
+#endif
