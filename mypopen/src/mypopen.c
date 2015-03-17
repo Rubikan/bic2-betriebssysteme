7e013ac38ce562721e4e826fc41fe715ea1bc4d7
Rubikan <rubikandreas@gmail.com>
2015-03-17 21:01:53 +0100

    added fork and dup, work in progress

--- mypopen/src/mypopen.c
+++ mypopen/src/mypopen.c
@@ -19,16 +19,30 @@ FILE *popen(const char *command, const char *type) {
   int pipefd[2];
   pid_t pid;
 
+  if (*type != 'w' && *type != 'r') {
+    errno = EINVAL;
+    return NULL;
+  }
   if (pipe(pipefd) == -1) {
-    /*TODO: Error handling*/
+    return NULL;
   }
   pid = fork(void);
   if (pid == 0) {
-    /*TODO: Child process*/
+    switch(type) {
+      case 'w':
+        dup2(pdes[0], fileno(stdin));
+        close(pdex[0]);
+        break;
+      case 'r':
+        dup2(pdes[1], fileno(stdout));
+        close(pdex[1]);
+        break;
+    }
+    /*TODO: Execute command in child*/
   } else if (pid > 0) {
     /*TODO: Parent process*/
   } else if (pid < 0) {
-    /*TODO: Error handling*/
+    return NULL;
   }
 }
 
