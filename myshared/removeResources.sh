7e45241b52b01e15d0df45fffb50a9b87b67999d
Rubikan <rubikandreas@gmail.com>
2015-06-02 17:55:18 +0200

    removed scripts

--- myshared/removeResources.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-ipcs -sa | awk '/^m / { if ($9 == 0) { print $2 }}' | xargs -n 1 ipcrm -s
-ipcs -ma | awk '/^m / { if ($9 == 0) { print $2 }}' | xargs -n 1 ipcrm -m
-ipcs -qa | awk '/^m / { if ($9 == 0) { print $2 }}' | xargs -n 1 ipcrm -q
