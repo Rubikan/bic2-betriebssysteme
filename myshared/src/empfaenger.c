974bbbdb751966ee6ff1d45b8a96b3f2cce92de3
Rubikan <rubikandreas@gmail.com>
2015-06-05 23:20:19 +0200

    added removed %

--- myshared/src/empfaenger.c
+++ myshared/src/empfaenger.c
@@ -109,7 +109,7 @@ int main(int argc, char* argv[]) {
     /* Critical section begin */
     if (shmptr[aktuellesEl % buffersize] != 256) {
       ch = shmptr[aktuellesEl % buffersize];
-      shmptr[aktuellesEl  buffersize] = '\0';
+      shmptr[aktuellesEl % buffersize] = '\0';
       if (putc(ch, stdout) == -1) {
         printf("Fehler beim Schreiben auf STDOUT! Errno: %d", errno);
         exit(EXIT_FAILURE);
