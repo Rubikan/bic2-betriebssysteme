494092b5ffbde001d44dd87c2dd27b3f03e3248d
Rubikan <rubikandreas@gmail.com>
2015-06-05 19:25:55 +0200

    changed the remove resources script

--- myshared/scripts/removeResources.sh
+++ myshared/scripts/removeResources.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
-ipcs -sa | awk '/^m / { if ($9 == 0) { print $2 }}' | xargs -n 1 ipcrm -s
-ipcs -ma | awk '/^m / { if ($9 == 0) { print $2 }}' | xargs -n 1 ipcrm -m
-ipcs -qa | awk '/^m / { if ($9 == 0) { print $2 }}' | xargs -n 1 ipcrm -q
+ME=`whoami`
+
+IPCS_S=`ipcs -s | egrep "0x[0-9a-f]+ [0-9]+" | grep $ME | cut -f2 -d" "`
+IPCS_M=`ipcs -m | egrep "0x[0-9a-f]+ [0-9]+" | grep $ME | cut -f2 -d" "`
+IPCS_Q=`ipcs -q | egrep "0x[0-9a-f]+ [0-9]+" | grep $ME | cut -f2 -d" "`
+
+
+for id in $IPCS_M; do
+  ipcrm -m $id;
+done
+
+for id in $IPCS_S; do
+  ipcrm -s $id;
+done
+
+for id in $IPCS_Q; do
+  ipcrm -q $id;
+done
